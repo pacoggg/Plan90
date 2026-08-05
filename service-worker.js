16bf0dc986a70813d367db1a2324780359c9cdce
Paco G. <pacoggg@users.noreply.github.com>
2026-08-05 10:12:30 +0200

    Añade funcionamiento sin conexión

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE = 'plan90-v0.1';
+const ASSETS = ['./','index.html','styles.css','app.js','manifest.json','icons/icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
